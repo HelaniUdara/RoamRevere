5b202867a2dde167b45c5ff861cdda6d69712a4e
Helani Seekkubadu <seekkub-se19022@stu.kln.ac.lk>
2024-01-06 23:01:14 +0530

    Created post UI component

--- frontend/src/components/Posts/Post/Post.js
+++ frontend/src/components/Posts/Post/Post.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@mui/material';
+import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
+import DeleteIcon from '@mui/icons-material/Delete';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import moment from 'moment';
 
 
-const Post = () => {
+const Post = ({post}) => {
   return (
-    <div>Post</div>
+    <Card>
+      <CardMedia image={post.selectedFile} title={post.title}/>
+      <div>
+        <Typography variant='h6'>{post.creator}</Typography>
+        <Typography variant='body2'>{moment(post.CreatedAt).fromNow()}</Typography>
+      </div>
+      <div>
+        <Button size='small' color='primary' onClick={()=>{}}>
+          <MoreHorizIcon fontSize='default'/>
+        </Button>
+      </div>
+      <div>
+        <Typography variant='body2' color='textSecondary'>{post.tags.map((tag)=>`#${tag} `)}</Typography>
+      </div>
+      <CardContent>
+              <Typography variant='h5' gutterBottom>{post.message}</Typography>
+      </CardContent>
+      <CardActions>
+        <Button size='small' color='primary' onClick={()=>{}}>
+          <ThumbUpAltIcon fontSize='small'/>
+          Like
+          {post.likeCount}
+        </Button>
+        <Button size='small' color='primary' onClick={()=>{}}>
+          <DeleteIcon fontSize='small'/>
+          Delete
+        </Button>
+    </CardActions>
+    </Card>
   )
 }
 
--- frontend/src/components/Posts/Posts.js
+++ frontend/src/components/Posts/Posts.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import Post from './Post/Post.js';
+import {Grid, CircularProgress} from '@mui/material';
 import { useSelector } from 'react-redux';
 
 const Posts = () => {
   const posts = useSelector((state)=> state.posts);
   console.log(posts);
   return (
-    <>
-        <h1>Posts</h1>
-        <Post/>
-        <Post/>
-    </>
+    !posts.length ? <CircularProgress /> : (
+      <Grid container alignItems='stretch' spacing={3}>
+        {posts.map((post)=>(
+          <Grid key={post._id} item xs={12} sm={6}>
+            <Post post={post}/>
+          </Grid>
+        ))}
+      </Grid>
+    )
   )
 }
 
